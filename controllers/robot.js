905615ca3c079ae7306f16c7bc851d7e4825ac71
Pablo <pablo.ruiz.andino@protonmail.com>
2024-04-01 16:55:41 +0200

    vitesse ajustement

--- controllers/robot.js
+++ controllers/robot.js
@@ -33,8 +33,8 @@ const robot = {
     camera:'Offline',
     direction:'DOWN',
     intern_vitesse:Math.round(50*2.55),
-    intern_vitesse_reduced: Math.round(50*2.55*0.88),
-    intern_vitesse_reduced_down: Math.round(50*2.55*0.95)
+    intern_vitesse_reduced: Math.round(50*2.55*0.72),
+    intern_vitesse_reduced_down: Math.round(50*2.55*0.10)
 }
 exports.getStatus = (req,res,next) => {
     res.status(200).json({robot: { status : robot.status, vitesse:robot.vitesse,batterie:robot.batterie,camera:robot.camera,direction:robot.direction} });
@@ -52,7 +52,7 @@ exports.handleCmd = (req,res,next) => {
             robot.vitesse = Math.round(req.body.cmd)
             robot.intern_vitesse =  Math.round(robot.vitesse * 2.55)
             robot.intern_vitesse_reduced = Math.round(robot.intern_vitesse*0.72)
-            robot.intern_vitesse_reduced_down = Math.round(robot.intern_vitesse*0.30)
+            robot.intern_vitesse_reduced_down = Math.round(robot.intern_vitesse*0.10)
             console.log(robot.intern_vitesse)
 
             if(robot.direction == "LEFT")
